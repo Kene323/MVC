95ffd2cb33660eb6e0b4f1766a766916e7cf7d37
Kene323 <ezemma789@gmail.com>
2025-02-18 00:41:37 -0600

    update

--- controller/userController.js
+++ controller/userController.js
@@ -1,6 +1,7 @@
 const userModel = require("../model/userModel")
 const bcrypt = require("bcrypt")
 
+//      GET ALL USERS
 const getAllUser = async (req, res) => {
     try {
         const allUsers = await userModel.find()
@@ -10,6 +11,24 @@ const getAllUser = async (req, res) => {
     }
 }
 
+//      USER-LOGIN
+const userLogin = async (req, res) => {
+    try { 
+        const {email, password} = req.body
+        const checkUser = await userModel.findOne({email})
+
+        if(!checkUser || checkUser.password !== password) {
+            res.status(404).json({message: "Invalid email or password"})
+        }
+         else {
+            res.status(200).json({message: "Welcome!!", user: {_id: checkUser._id, name: checkUser.name, email: checkUser.email}})
+        }
+    } catch (err) {
+        res.status(500).json({status: false, error: err.message})
+    }
+}
+
+//      GET ONE USER
 const getOneUser = async (req, res) => {
     try {
         const oneUser = await userModel.findById(req.params.id)
@@ -24,6 +43,7 @@ const getOneUser = async (req, res) => {
     }
 }
 
+//      CREATE A USER
 const createUser = async (req, res) => {
     try{
     const {name, password, email} = req.body
@@ -44,4 +64,52 @@ const createUser = async (req, res) => {
     }    
 }
 
-module.exports = {getAllUser, getOneUser, createUser}
+//      UPDATE A USER
+const updateUser = async (req, res) => {
+    try {
+        const { name, password, email} = req.body
+        const update = await userModel.findByIdAndUpdate(
+            req.params.id,
+            {name, password, email},
+            {new: true}
+        )
+
+        if(!update) {
+            res.status(404).json({message: "User not found"})
+        } else {
+            res.status(200).json({message: "User Updated", data: update})
+        }
+    }catch(error) {
+        res.status(500).json({message: "An error occured", error})
+    }
+}
+
+//       DELETE A USER
+const deleteOneUser = async (req, res) => {
+    try {
+        const deleteUser = await userModel.findByIdAndDelete(req.params.id);
+        if (!deleteUser) {
+          res.status(404).json({ message: "user not found" });
+        }
+        res.status(200).json({message: "user deleted", data: deleteUser});
+      } catch (error) {
+        res.status(500).json({ message: "An error occured", error });
+      }
+}
+
+//      DELETE ALL USERS
+const deleteAllUsers = async (req, res) => {
+    try {
+       const deleteTheUsers = await userModel.deleteMany();
+    
+        if(!deleteAllUsers) {
+            res.status(404).json({message: "No user found"})
+        }
+        res.status(200).json({message: "all user deleted"});
+
+      } catch (error) {
+        res.status(500).json({ status: false, error: error.message });
+      }
+}
+
+module.exports = {getAllUser, getOneUser, createUser, updateUser, userLogin, deleteOneUser, deleteAllUsers}
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,10 +1,17 @@
 const express = require("express")
-const { getAllUser, getOneUser, createUser } = require("../controller/userController")
+const { getAllUser, getOneUser, createUser, updateUser, userLogin, deleteOneUser, deleteAllUsers} = require("../controller/userController")
 
 const routes = express.Router()
 
 routes.get("/", getAllUser)
+routes.get("/login", userLogin)
 routes.get("/:id", getOneUser)
 
 routes.post("/create", createUser)
+
+routes.patch("/:id", updateUser)
+
+routes.delete("/:id", deleteOneUser)
+routes.delete("/delete", deleteAllUsers)
+
 module.exports = routes
